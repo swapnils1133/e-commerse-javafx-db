60175c51163832182639ad51f1aaef5c5a3aaad4
swapnils1133 <swapnilsakhare1133@gmail.com>
2024-06-24 18:26:54 +0530

    first commit

---
+++ src/login/LoginScreenController.java
@@ -0,0 +1,39 @@
+package login;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import common.DBUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+public class LoginScreenController {
+	
+	@FXML
+	TextField loginName;
+	
+	@FXML
+	TextField password;
+	
+	@FXML
+	Label errorMessage;
+
+	public  void loginButtonClick() throws SQLException {
+		
+		String userName = loginName.getText();
+		String passwords = password.getText();
+		
+		String selectQuery = "select * from user where  first_name= '"+userName+"' && password='"+passwords+"' ";
+		ResultSet resultset = DBUtil.executeSelectQuery(selectQuery);
+		if(resultset.next()) {
+			errorMessage.setText("Login Successfully !.....");
+			errorMessage.setTextFill(Color.GREEN);
+		}else {
+			errorMessage.setText("Login Failed !!!!");
+			errorMessage.setTextFill(Color.RED);
+		}
+	}
+
+}
